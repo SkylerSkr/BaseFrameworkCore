e9dfb1a8b17f7c192075da4b5ccaa102f952ea7b
SkylerSkr <1056375224@qq.com>
2019-07-31 11:41:18 +0800

    feature/多数据库支持

--- Base.Repository/DBProxy.cs
+++ Base.Repository/DBProxy.cs
@@ -4,15 +4,16 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 using Base.Common.Config;
+using Base.Domain.Enum;
 
 namespace Base.Repository
 {
     internal class DBProxy
     {
-        internal static IDbConnection CreateClient()
+        internal static IDbConnection CreateClient(EnumDBType? dbType)
         {
-           var connString= Appsettings.app("SkrConnectString");
-           return new SqlConnection(connString);
+            var connString = Appsettings.app($"{dbType ?? EnumDBType.Japan}ConnectString");
+            return new SqlConnection(connString);
         }
     }
 }
